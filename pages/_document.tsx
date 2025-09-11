97eadeec2cb4cea387e9789f00f0849a15458a09
shashanka2a <37143174+shashanka2a@users.noreply.github.com>
2025-09-11 12:55:34 -0400

    update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,11 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="description" content="SnapEvent - Connect with Professional Photographers" />
-        <meta name="theme-color" content="#030213" />
+        <meta name="theme-color" content="#6366f1" />
+        <meta name="msapplication-TileColor" content="#6366f1" />
       </Head>
       <body>
         <Main />
